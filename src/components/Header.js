97b971785ea19504ec085af181a845648a472a6e
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-21 13:29:19 -0400

    creates nav links and fetchs data from backend

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
     return (
         <nav className="nav">
             <Link to="/">
-                <div>People Add</div>
+                <div>People App</div>
             </Link>
         </nav>
     );
